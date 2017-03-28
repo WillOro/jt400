4127765bd9517898f68a22945a36b2ac5b9cdf22
William Otoniel <william.otoniel@gmail.com>
2017-03-28 10:48:15 -0600

    prueba

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-module.exports = require('./lib/jt400');
+static Properties props;
+
+var local_system=130.1.1.66;
+var userId=USR_DOCKER;
+var password=DOCKER1;
+
+try {
+
+ props = new Properties();
+ props.load(new FileInputStream("properties/mydb2.properties"));
+
+ String DRIVER = "com.ibm.as400.access.AS400JDBCDriver"; 
+ String URL = "jdbc:as400://" + props.getProperty("local_system").trim() + ";naming=system;errors=full";
+ Connection conn = null;
+
+ //Connect to iSeries 
+ Class.forName(DRIVER); 
+ conn = DriverManager.getConnection(URL, props.getProperty("userId").trim(), props.getProperty("password").trim()); 
+
+ conn.close();
+
+}
+catch (Exception e) {
+ System.out.println(e);
+}
