8745a63c9a6a9ff188c730452d40dc12c789bd26
William Otoniel <william.dgac@gmail.com>
2020-01-08 13:37:23 -0600

    Update select.js

--- example/select.js
+++ example/select.js
@@ -4,13 +4,13 @@ var database = new Database();
 var config = {
   libpath: __dirname + '/jt400.jar',
   drivername: 'com.ibm.as400.access.AS400JDBCDriver',
-  url: 'jdbc:as400://130.1.1.66/Target;user=USR_DOCKER;password=DOCKER1'
+  url: 'jdbc:as400://ipaddress/Target;user=USR;password=DCKR#'
 };
 
 database.initialize(config);
 
 // SELECT statements must be run with execute()
-database.execute('SELECT * FROM QSTCDAT.G3PLAN');
+database.execute('SELECT * FROM SCHEMA.TABLE');
 
 database.on('execute', function(error, results) {
   if (error) {
